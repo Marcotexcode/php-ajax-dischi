12bdfba8afa3e6fff6052944becfc0f494cd2f2a
Marco Tessitore <marcotexcode@gmail.com>
2021-07-28 15:07:23 +0200

    es. completato

--- index-php.php
+++ index-php.php
@@ -4,8 +4,6 @@
 
     include __DIR__  . '/db.php';
 
-    var_dump($dischi[0]['title'])
-
 ?>
 
 
@@ -15,6 +13,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
@@ -30,20 +29,24 @@
         <div class="corpo">
 
             <?php  foreach($dischi as $disco) { ?>
+     
+                <div class="container-box">
 
-            <div class="box">
+                    <img class="box--poster" src="<?php echo  $disco['poster']  ?>" alt="">
 
-                <img class="box--poster" src="<?php echo  $disco['poster']  ?>" alt="">
+                    <div class="textBox">
 
-                <h2 class="box--title" ><?php echo  $disco['title']  ?></h2>
+                        <h2 class="box--title"><?php echo  $disco['title']  ?></h2>
 
-                <h4 class="box--author" ><?php echo  $disco['author']  ?></h4>
+                        <h4 class="box--author" >Autore: <?php echo  $disco['author']  ?></h4>
 
-                <span class="box--year" ><?php echo  $disco['year']  ?></span>
+                        <span class="box--year">Anno: <?php echo  $disco['year']  ?></span>
 
-            </div>
+                    </div>
 
-            <?php } ?>
+                </div>  
+                
+            <?php  } ?>
 
         </div>
 
