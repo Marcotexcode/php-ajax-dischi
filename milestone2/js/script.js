5953c5195a3dec70d9f5a0191a4dfe0e1e6983bc
Marco Tessitore <marcotexcode@gmail.com>
2021-07-29 13:12:33 +0200

    chiamata axios

---
+++ milestone2/js/script.js
@@ -0,0 +1,31 @@
+const app = new Vue({
+
+    el: '#app',
+
+    data: {
+
+        url: 'api/api-album.php',
+
+        album: ''
+
+    },
+
+    mounted() {
+
+        axios.get(this.url).then(response => {
+
+            this.album = response.data;
+
+            console.log(this.album);
+
+        })
+
+        .catch(error => {
+
+            console.log(error);
+
+        }); 
+
+    }
+
+});
